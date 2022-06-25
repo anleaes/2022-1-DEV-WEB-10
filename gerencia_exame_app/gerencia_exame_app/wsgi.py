212e1fa8eef33bc0726183b991346ca6ae615db2
RoniTeles <ronaldo.teles1996@gmail.com>
2022-06-24 21:16:36 -0300

    Ambiente montado novamente e conexão com banco

--- gerencia_exame_app/gerencia_exame_app/wsgi.py
+++ gerencia_exame_app/gerencia_exame_app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for exame_app project.
+WSGI config for gerencia_exame_app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exame_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gerencia_exame_app.settings')
 
 application = get_wsgi_application()
